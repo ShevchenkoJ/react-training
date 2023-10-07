b7e5be3a6e735c63e8e60ad7cc4046d0323a45ef
ShevchenkoJ <shevchenko.juliia@gmail.com>
2023-10-07 17:51:10 +0200

    add components to app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import {useState} from 'react';
+import {useEffect} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -15,10 +18,41 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React (1)
         </a>
       </header>
+      <MyButton />
+      <Profile />
     </div>
+  );
+}
+
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 80,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
   );
 }
 
