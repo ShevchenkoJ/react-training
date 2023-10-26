a7372e54dc351b7c9111d9ad6d6a6c4f83862215
ShevchenkoJ <shevchenko.juliia@gmail.com>
2023-10-26 18:23:32 +0200

    update paths

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import {useState, useEffect} from 'react';
 
 import logo from './logo.svg';
 import './App.css';
-import Profile, {user} from './components/Profile';
-import MyButton from './components/MyButton';
-import ShoppingList from './components/ShoppingList';
+import Profile, {user} from './components/Profile/Profile';
+import ConfirmationButton from './components/ConfirmationButton/ConfirmationButton';
+import ShoppingList from './components/ShoppingList/ShoppingList';
 
 function App() {
+  // let btnText = '';
+  // The goal: get the button text each time it gets changed. 
+  // Watch it and update it on the App() level.
   return (
     <div className="App">
       <header className="App-header">
+        {/* {btnText} */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -24,7 +28,7 @@ function App() {
           Learn React (1)
         </a>
       </header>
-      <MyButton />
+      <ConfirmationButton />
       <Profile />
       <ShoppingList />
     </div>
