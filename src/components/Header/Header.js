1458bfdb2c95277af90a5f64db87efe280beb20c
ShevchenkoJ <shevchenko.juliia@gmail.com>
2023-10-29 15:26:25 +0100

    add header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import logo from '../../logo.svg';
+import Profile, {user} from '../Profile/Profile';
+
+function Header() {
+    return (
+    <header className="App-header">
+        {/* {btnText} */}
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>Test of export: {user.name}</p>
+        <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Learn React (1)
+        </a>
+    </header>
+    );
+}
+
+export default Header;
