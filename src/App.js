ea9b546adfe1e0c225b25cea21d6f446936f6478
ShevchenkoJ <shevchenko.juliia@gmail.com>
2023-11-02 13:54:39 +0100

    update app js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+// import {useState, useEffect} from 'react';
 
 import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
