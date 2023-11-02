c5c67a2ca48b7fd6e52c01e5b9c5256841f69a62
ShevchenkoJ <shevchenko.juliia@gmail.com>
2023-11-02 13:54:58 +0100

    add clicks count to button

--- src/components/ConfirmationButton/ConfirmationButton.js
+++ src/components/ConfirmationButton/ConfirmationButton.js
@@ -4,13 +4,19 @@ import './ConfirmationButton.css';
 function ConfirmationButton() {
 
   const [buttonText, setButtonText] = useState('Click me');
+  const [count, setCount] = useState(0);
 
   const handleClick = () => {
     setButtonText('Confirmation button clicked');
+    setCount(count + 1);
   };
     return (
       <div className='confirmation-button_wrapper'>
-        <button className='confirmation-button' onClick={handleClick}>{buttonText}</button>
+        <button 
+        className='confirmation-button' 
+        onClick={handleClick}>
+          {buttonText}, Clicked {count} times
+        </button>
       </div>
     );
 }
